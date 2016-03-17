520555accc2684144de00291169f542a362a1029
crayzeejay <crayzeejay92@hotmail.com>
2016-03-17 19:30:00 +1000

    Added web API

---
+++ Awesome Angular Web App 2.0/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Awesome_Angular_Web_App_2._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
---
+++ Awesome Angular Web App 2.0/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Awesome_Angular_Web_App_2._0.Startup))]
+
+namespace Awesome_Angular_Web_App_2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
